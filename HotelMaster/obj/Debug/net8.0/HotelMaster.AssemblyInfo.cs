0d7f31a6ba1345e61a7b1eb8c7badd3acc715b72
Vikash <Vikash@DESKTOP-41QTDFT>
2026-08-07 12:00:28 +0530

    merge code from master

--- HotelMaster/obj/Debug/net8.0/HotelMaster.AssemblyInfo.cs
+++ HotelMaster/obj/Debug/net8.0/HotelMaster.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelMaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+817abc5c435eb8cb8b3979affff96acc80dd9b01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42051767cd88eeb8f53bed644c315b05a8f33ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelMaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelMaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
